40130198e6659bde9557d3536d3754b522662ae5
Kennion Gubler <gublerkennion@gmail.com>
2021-06-22 14:46:20 -0600

    Added sign up button and sign in text.

--- Sunset Sounds Final/ContentView.swift
+++ Sunset Sounds Final/ContentView.swift
@@ -29,15 +29,54 @@ struct ContentView: View {
     var body: some View {
         
         TabView {
+            //Rectangle 1
+            VStack {
+                if sizeClass == .compact{
+                    
+                    
+                    ZStack{
+                        Color(red: 23 / 255, green: 30 / 255, blue: 45 / 255).edgesIgnoringSafeArea(.all)
+                        //Sign Up
+                                
+
+                        //Rectangle 1
+                            RoundedRectangle(cornerRadius: 30)
+                            .fill(Color(#colorLiteral(red: 0.9490196108818054, green: 0.6352941393852234, blue: 0.364705890417099, alpha: 1)))
+                            .frame(width: 303, height: 79)
+                            .shadow(color: Color(#colorLiteral(red: 0.2541666626930237, green: 0.23298610746860504, blue: 0.23298610746860504, alpha: 0.25)), radius: 1, x: 3, y: 3)
+                                .offset(y:175)
+                        //Sign Up
+                            Text("Sign Up").font(.system(size: 20, weight: .medium, design: .rounded)).foregroundColor(Color(.black)).offset(y:175)
+                        //Have an Account? Sign In
+                        Text("Have an Account?" + " Sign In").font(.system(size: 14, weight: .regular, design: .rounded)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).offset(y:250)
+                            
+                            
+                        
+                        }
+                    
+                    
+                    
+                    
+                }
+                
+                    
+            }
+            
+
+            
             
             Login_Screen()
                 .tabItem{
+                    
                     Image(systemName: "ellipsis")
                     Text("More")
+                    
                        
                 }
             
         }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        
         
             
             
@@ -148,6 +187,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
     }
 }
 }
